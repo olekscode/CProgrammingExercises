cca1f4e4f47449e47e3b5742c962f0217207a9b7
Oleksandr Zaytsev <olk.zaytsev@gmail.com>
2019-09-20 02:05:38 +0200

    Started writing exercises about printf and scanf

---
+++ P1-BasicSyntax/ch2-input-output/ex1-printf.c
@@ -0,0 +1,41 @@
+/* To understand what is happening inside your program, you need to get
+some form of output. The easiest way to do that is to print values to the
+Terminal. In this exercise, you will learn to use the "printf" function
+from the standard library. We will only cover the most important
+features of the "printf" function that will be useful for you in this course.
+But we strongly encourage you to go on the Internet and read more about
+different flags and specifications of this wonderful function.
+
+Task
+===========
+
+1. The easienst way to use the "printf" function is to give it a string
+of text that you want to be printed. You have already seen this in previous
+exercises.
+
+  printf("Hello, world!\n");
+  printf("My message\n");
+
+Add a line to the code in the bottom of this file that prints your name
+to the Terminal.
+
+2. You already know that \n is a newline character. It is a special symbol
+that can be inserted anywhere in your string to mark the beginning of a new
+line. There are many other special characters that can be used in strings.
+The most important ones that you need to know are:
+
+  \n - newline
+  \t - tabulation
+
+and just for fun, you can try the \a character - it will make a "bip" sound :)
+
+TODO: Finish this
+*/
+
+#include <stdio.h>
+
+int main() {
+  // Write your code here
+
+  return 0;
+}
